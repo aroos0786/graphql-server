ce76d4e8bc7f81750fb76d00267f278bc0982225
Aroos Afzal <aroosafzal0786@gmai.com>
2021-07-28 14:19:32 -0700

    updates

---
+++ src/db.js
@@ -0,0 +1,8 @@
+export let users = [
+    {
+        id: 1, name: "Aroos Afzal", email: "aroos@gmail.com", age: 23 
+    },
+    {
+        id: 2, name: "Usama Saeed", email: "usama@gmail.com", age: 24
+    }
+]
